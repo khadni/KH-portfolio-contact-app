3b2e4aa02c13ff22eca217c3dfb3ed334bb6ba91
Karim Hadni <karim.hadni@gmail.com>
2022-03-20 12:07:54 +0100

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,12 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "José",
-      email: "jose@gmail.com",
-    },
-    {
-      id: "2",
-      name: "Jules",
-      email: "jules@gmail.com",
-    },
-    {
-      id: "3",
-      name: "Karim",
-      email: "karim@gmail.com",
-    },
-  ];
+  const [contacts, setContacts] = useState([]);
 
   return (
     <div className="font-poppins">
